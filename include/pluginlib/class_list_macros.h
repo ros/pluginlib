7d10fff63bcf169e880904951c1c5465d876e21a
William Woodall <william+github@osrfoundation.org>
2017-10-06 14:06:48 -0700

    use @ instead of \ in doxygen commands for now

--- include/pluginlib/class_list_macros.h
+++ include/pluginlib/class_list_macros.h
@@ -41,8 +41,8 @@
 
 /// Register a class with class loader to effectively export it for plugin loading later.
 /**
- * \param class_type The real class name with namespace qualifier (e.g. Animals::Lion)
- * \param base_class_type The real base class type from which class_type inherits
+ * @param class_type The real class name with namespace qualifier (e.g. Animals::Lion)
+ * @param base_class_type The real base class type from which class_type inherits
  */
 #define PLUGINLIB_EXPORT_CLASS(class_type, base_class_type) \
   CLASS_LOADER_REGISTER_CLASS(class_type, base_class_type);
