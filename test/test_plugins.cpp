4d5073575d6e77d2db5cd6737f8f6f20432aa199
Mikael Arguedas <mikael@osrfoundation.org>
2017-10-25 13:24:48 -0700

    remove nolint and use relative paths

--- test/test_plugins.cpp
+++ test/test_plugins.cpp
@@ -28,8 +28,8 @@
  */
 
 #include <pluginlib/class_list_macros.h>
-#include "test_base.h"  // NOLINT
-#include "test_plugins.h"  // NOLINT
+#include "./test_base.h"
+#include "./test_plugins.h"
 
 PLUGINLIB_DECLARE_CLASS(pluginlib, foo, test_plugins::Foo, test_base::Fubar)
 PLUGINLIB_DECLARE_CLASS(pluginlib, bar, test_plugins::Bar, test_base::Fubar)
