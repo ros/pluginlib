341b708e23b42c95b016b9f5178f0b2af9ff0ce1
Mikael Arguedas <mikael@osrfoundation.org>
2017-12-05 12:36:45 -0800

    typo

--- pluginlib-extras.cmake
+++ pluginlib-extras.cmake
@@ -29,8 +29,8 @@ endif()
 # tinyxml2 is being exported as a dependency of pluginlib, but
 # with newer versions of tinyxml2 (>=5.0.1), the tinyxml2_LIBRARIES variable is empty
 # and instead the tinyxml2 exported CMake target exists.
-# However, ament_export_dependencies will not propogate this, so here we will
-# check for tinyxml2 the exported target and if found we will add it to the
+# However, ament_export_dependencies will not propagate this, so here we will
+# check for the tinyxml2 exported target and if found we will add it to the
 # pluginlib_LIBRARIES.
 if(TARGET tinyxml2)
   list(APPEND pluginlib_LIBRARIES tinyxml2)
