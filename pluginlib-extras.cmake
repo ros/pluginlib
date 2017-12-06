3ce2da97c273363bd348a5f9a30772566ae82d2e
William Woodall <william@osrfoundation.org>
2017-12-05 18:41:10 -0800

    yet more changes to how tinyxml2 is used

--- pluginlib-extras.cmake
+++ pluginlib-extras.cmake
@@ -25,14 +25,3 @@ if(CMAKE_COMPILER_IS_GNUCXX)
   # ament_export_libraries() because it is not absolute and cannot be found with find_library
   list(APPEND pluginlib_LIBRARIES stdc++fs)
 endif()
-
-# tinyxml2 is being exported as a dependency of pluginlib, but
-# with newer versions of tinyxml2 (>=5.0.1), the tinyxml2_LIBRARIES variable is empty
-# and instead the tinyxml2 exported CMake target exists.
-# However, ament_export_dependencies will not propagate this, so here we will
-# check for the tinyxml2 exported target and if found we will add it to the
-# pluginlib_LIBRARIES.
-find_package(tinyxml2 REQUIRED QUIET)
-if(TARGET tinyxml2)
-  list(APPEND pluginlib_LIBRARIES tinyxml2)
-endif()
