3bbc242f1cbdeb9a51375fb8941deb386dbf9f2f
Emerson Knapp <537409+emersonknapp@users.noreply.github.com>
2019-03-21 11:12:37 -0700

    Choose the appropriate library for experimental filesystem, based on the compiler and standard library (#146)
    Signed-off-by: Emerson Knapp <eknapp@amazon.com>

--- pluginlib/pluginlib-extras.cmake
+++ pluginlib/pluginlib-extras.cmake
@@ -20,8 +20,18 @@ ament_register_extension("ament_package" "pluginlib"
 
 include("${pluginlib_DIR}/pluginlib_export_plugin_description_file.cmake")
 
+if(CMAKE_CXX_COMPILER_ID MATCHES "Clang" AND CMAKE_CXX_FLAGS MATCHES "-stdlib=libc\\+\\+")
+  if(CMAKE_CXX_COMPILER_VERSION VERSION_LESS 7.0)
+    set(FILESYSTEM_LIB c++experimental)
+  else()
+    set(FILESYSTEM_LIB c++fs)
+  endif()
+else()
+  set(FILESYSTEM_LIB stdc++fs)
+endif()
+
 if(UNIX AND NOT APPLE)
   # this is needed to use the experimental/filesystem on Linux, but cannot be passed with
   # ament_export_libraries() because it is not absolute and cannot be found with find_library
-  list(APPEND pluginlib_LIBRARIES stdc++fs)
+  list(APPEND pluginlib_LIBRARIES ${FILESYSTEM_LIB})
 endif()
