df53950f57d40f5a12f86700f7337fc29d73494e
Sean Yen <seanyen@microsoft.com>
2020-05-08 00:45:30 -0700

    [ros2] Export tinyxml2 directly from pluginlib-extras.cmake

--- pluginlib/pluginlib-extras.cmake
+++ pluginlib/pluginlib-extras.cmake
@@ -45,9 +45,4 @@ find_package(tinyxml2_vendor REQUIRED)
 find_package(TinyXML2 REQUIRED)
 list(APPEND pluginlib_LIBRARIES ${TinyXML2_LIBRARIES})
 
-add_library(tinyxml2_vendor INTERFACE)
-target_include_directories(tinyxml2_vendor INTERFACE
-  ${TinyXML2_INCLUDE_DIRS})
-target_link_libraries(tinyxml2_vendor INTERFACE
-  ${TinyXML2_LIBRARIES})
-list(APPEND pluginlib_TARGETS tinyxml2_vendor)
+list(APPEND pluginlib_TARGETS tinyxml2)
