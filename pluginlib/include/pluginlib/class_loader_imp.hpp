6cae99209d09bd9b7514ef2c9f968c27ade82579
Shane Loretz <sloretz@openrobotics.org>
2021-12-07 07:38:00 -0800

    Require <memory> (#225)
    Signed-off-by: Shane Loretz <sloretz@osrfoundation.org>

--- pluginlib/include/pluginlib/class_loader_imp.hpp
+++ pluginlib/include/pluginlib/class_loader_imp.hpp
@@ -40,28 +40,12 @@
 #include <cstdlib>
 #include <list>
 #include <map>
+#include <memory>
 #include <sstream>
 #include <stdexcept>
 #include <string>
 #include <utility>
 #include <vector>
-
-/* This is a workaround to MSVC incorrectly reporting the __cplusplus version
- * as explained in:
- * https://blogs.msdn.microsoft.com/vcblog/2018/04/09/msvc-now-correctly-reports-__cplusplus/
- *
- * I'm hesitant to currently switch on the /Zc:__cplusplus switch, as there are
- * reports of code (incorrectly) assuming it should always be set to 199711L.
- */
-#if defined(_MSC_VER)
-# define HAS_CPP11_MEMORY (_MSC_VER >= 1900)
-#else
-# define HAS_CPP11_MEMORY (__cplusplus >= 201103L)
-#endif
-
-#if defined(HAS_CPP11_MEMORY) && HAS_CPP11_MEMORY
-# include <memory>
-#endif
 
 #include "ament_index_cpp/get_package_prefix.hpp"
 #include "ament_index_cpp/get_package_share_directory.hpp"
@@ -160,16 +144,13 @@ T * ClassLoader<T>::createClassInstance(const std::string & lookup_name, bool au
   }
 }
 
-#if defined(HAS_CPP11_MEMORY) && HAS_CPP11_MEMORY
 template<class T>
 std::shared_ptr<T> ClassLoader<T>::createSharedInstance(const std::string & lookup_name)
 /***************************************************************************/
 {
   return createUniqueInstance(lookup_name);
 }
-#endif
 
-#if defined(HAS_CPP11_MEMORY) && HAS_CPP11_MEMORY
 template<class T>
 UniquePtr<T> ClassLoader<T>::createUniqueInstance(const std::string & lookup_name)
 {
@@ -201,7 +182,6 @@ UniquePtr<T> ClassLoader<T>::createUniqueInstance(const std::string & lookup_nam
     throw pluginlib::CreateClassException(ex.what());
   }
 }
-#endif
 
 template<class T>
 T * ClassLoader<T>::createUnmanagedInstance(const std::string & lookup_name)
